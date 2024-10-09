6c6f9b81c7de523d82c1f27ff5bf1a03d63ec282
mohammad-khs <101863197+mohammad-khs@users.noreply.github.com>
2024-10-09 22:23:24 +0330

    eslint error free

--- src/components/customNeshanMap.tsx
+++ src/components/customNeshanMap.tsx
@@ -2,9 +2,9 @@
 import { FC } from "react";
 import NeshanMap from "@neshan-maps-platform/react-openlayers";
 import "@neshan-maps-platform/react-openlayers/dist/style.css";
-interface CustomNeshanMapProps {}
+// interface CustomNeshanMapProps {}
 
-const CustomNeshanMap: FC<CustomNeshanMapProps> = () => {
+const CustomNeshanMap: FC = () => {
   return (
     <>
       <NeshanMap mapKey={"web.91315c623fc243238e5679a3e2a0da09"}></NeshanMap>
