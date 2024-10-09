5c0dcdafc409d7dd14bd23f434dbae04e359942e
mohammad-khs <101863197+mohammad-khs@users.noreply.github.com>
2024-10-09 22:21:43 +0330

    test for neshanMap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@
 
 import { AuroraBackgroundDemo } from "@/components/customAuroraBackground";
 
+import dynamic from "next/dynamic";
+const DynamicNeshanMap = dynamic(() => import("@/components/customNeshanMap"), {
+  ssr: false,
+  loading: () => <div>Loading ...</div>,
+});
 export default function Home() {
   return (
     <main className="bg-slate-950">
       <AuroraBackgroundDemo  />
+      <DynamicNeshanMap />
     </main>
   );
 }
---
+++ src/components/customNeshanMap.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { FC } from "react";
+import NeshanMap from "@neshan-maps-platform/react-openlayers";
+import "@neshan-maps-platform/react-openlayers/dist/style.css";
+interface CustomNeshanMapProps {}
+
+const CustomNeshanMap: FC<CustomNeshanMapProps> = () => {
+  return (
+    <>
+      <NeshanMap mapKey={"web.91315c623fc243238e5679a3e2a0da09"}></NeshanMap>
+    </>
+  );
+};
+
+export default CustomNeshanMap;
